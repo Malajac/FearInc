2d1488a5933ee46965139f3889d1f39d207ac1f9
Denis Gerina <ecarapa@yahoo.com>
2020-06-21 13:43:09 +0200

    News model: set field verbose names

--- news/models.py
+++ news/models.py
@@ -2,12 +2,12 @@ from django.db import models
 
 
 class News(models.Model):
-    title = models.CharField(max_length=150)
-    content = models.TextField(blank=True)
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
-    photo = models.ImageField(upload_to='photos/%Y/%m/%d/')
-    is_published = models.BooleanField(default=True)
+    title = models.CharField(max_length=150, verbose_name='News title')
+    content = models.TextField(blank=True, verbose_name='Content')
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Published on')
+    updated_at = models.DateTimeField(auto_now=True, verbose_name='Updated on')
+    photo = models.ImageField(upload_to='photos/%Y/%m/%d/', verbose_name='Photo')
+    is_published = models.BooleanField(default=True, verbose_name='Published?')
 
     def __str__(self):
         return self.title
