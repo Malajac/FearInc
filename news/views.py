11ceb8eeb6b40ffd3d7fd97c8ca4af675c8a624e
Denis Gerina <ecarapa@yahoo.com>
2020-07-02 06:46:57 +0200

    Create test GET JSON endpoint

--- news/views.py
+++ news/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from .models import News
 
+import json
+
 def index(request):
     news = News.objects.order_by('-created_at')
     context = {
@@ -10,5 +12,7 @@ def index(request):
     }
     return render(request, 'news/index.html', context)
 
+
 def test(request):
-    return HttpResponse('Test page')
+    response = json.dumps([{}])
+    return HttpResponse(response, content_type='text/json')
